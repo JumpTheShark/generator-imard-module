1e79d1c2dc7006bb7f9d23b50f8363b6fdc57679
Oobluzhok Nachasic <nachasic@gmail.com>
2016-10-03 16:40:23 +0300

    Updated the tests

--- test/app.js
+++ test/app.js
@@ -1,18 +1,43 @@
 'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
+const path = require('path'),
+  assert = require('yeoman-assert'),
+  helpers = require('yeoman-test');
 
-describe('generator-imard-module:app', function () {
+describe('Generating module: common case', function () {
+  const promtsEN = {
+    language : "en",
+    title : "Codified module",
+    description : "Codified description",
+    keywords : ["keyword1", "keyword2"],
+    contributors : ["contrib1", "contrib2"],
+    disciplines : ["disc1", "disc2"],
+    studyObjectives : ["so1", "so2", "so3"]
+  };
+
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts(promtsEN)
       .toPromise();
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'module.json',
+      'module.md',
+      'package.json',
+      '.gitignore',
+      'README.md'
     ]);
   });
+
+  it('populates files with suitable contents read from the prompts', function () {
+    assert.fileContent("README.md", new RegExp(`#\\s${promtsEN.title}`, 'g'));
+    assert.fileContent("README.md", new RegExp(`${promtsEN.description}`, 'g'));
+
+    assert.fileContent("module.json", new RegExp(`\"language\"\: \"${promtsEN.language}\"\,`, 'g'));
+  });
+});
+
+describe("Generating module: localization case", function () {
+
 });
