6d6136dc19d02354d417b2ba3ab611b24dd78ddd
Oobluzhok Nachasic <nachasic@gmail.com>
2016-10-03 16:52:37 +0300

    Updated test for 100% coverage (funciton-level)

--- test/app.js
+++ test/app.js
@@ -3,6 +3,11 @@ const path = require('path'),
   assert = require('yeoman-assert'),
   helpers = require('yeoman-test');
 
+const localeSamples = {
+  "en": "An educational module",
+  "ru": "Обучающий модуль"
+}
+
 describe('Generating module: common case', function () {
   const promtsEN = {
     language : "en",
@@ -35,9 +40,42 @@ describe('Generating module: common case', function () {
     assert.fileContent("README.md", new RegExp(`${promtsEN.description}`, 'g'));
 
     assert.fileContent("module.json", new RegExp(`\"language\"\: \"${promtsEN.language}\"\,`, 'g'));
+    assert.fileContent("README.md", new RegExp(`${localeSamples[promtsEN.language]}`, 'g'));
   });
 });
 
 describe("Generating module: localization case", function () {
+  const promtsRU = {
+    language : "ru",
+    title : "Кодицифированный модуль",
+    description : "Специальное описание",
+    keywords : ["ключевоеслово1", "ключевоеслово2"],
+    contributors : ["автор1", "автор2"],
+    disciplines : ["дисц1", "дисц2"],
+    studyObjectives : ["со1", "со2", "со3"]
+  };
 
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts(promtsRU)
+      .toPromise();
+  });
+
+  it('creates files', function () {
+    assert.file([
+      'module.json',
+      'module.md',
+      'package.json',
+      '.gitignore',
+      'README.md'
+    ]);
+  });
+
+  it("populates files with the proper info in their locale", function () {
+    assert.fileContent("README.md", new RegExp(`#\\s${promtsRU.title}`, 'g'));
+    assert.fileContent("README.md", new RegExp(`${promtsRU.description}`, 'g'));
+
+    assert.fileContent("module.json", new RegExp(`\"language\"\: \"${promtsRU.language}\"\,`, 'g'));
+    assert.fileContent("README.md", new RegExp(`${localeSamples[promtsRU.language]}`, 'g'));
+  });
 });
